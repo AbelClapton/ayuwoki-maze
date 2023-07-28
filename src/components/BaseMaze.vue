8024afde760710b2cd17ff9ea1d5b92152ea9c35
AbelClapton <abelrguezxd@gmail.com>
2023-07-28 14:35:53 -0400

    update imports

--- src/components/BaseMaze.vue
+++ src/components/BaseMaze.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import BaseCell from '@/components/BaseCell.vue'
-import BaseCharacter from '@/components/BaseCharacter.vue'
-import { type Maze as MazeProps } from '@/types'
-import { Maze } from '@/libraries/Maze'
+import { Maze } from '@/libraries'
+
+import { BaseCell, BasePlayer } from '@/components'
 import { usePlayer } from '@/composables'
+import { type Maze as MazeProps } from '@/types'
 
 const props = defineProps<MazeProps>()
 const maze = ref(new Maze(props.rows, props.cols))
@@ -13,8 +13,8 @@ const { player } = usePlayer({ x: 0, y: 0 }, maze)
 
 <template>
   <div class="maze">
-    <base-cell v-for="cell of maze.cells" :key="cell.x + cell.y * props.cols" :cell="cell" />
-    <base-character :x="player.x" :y="player.y" :size="props.cellSize" />
+    <base-cell v-for="(cell, idx) of maze.cells" :key="idx" :cell="cell" />
+    <base-player :x="player.x" :y="player.y" :size="props.cellSize" />
   </div>
 </template>
 
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import BaseMaze from '@/components/BaseMaze.vue'
+import { BaseMaze } from '@/components'
 </script>
 
 <template>
